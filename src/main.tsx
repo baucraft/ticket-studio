f6f8c22197878fd45f99af302fff319c71a90e70
Trong Tin Dinh <tin-trong.dinh@dreso.com>
2026-09-18 22:52:46 +0200

    fix: load SheetJS loader in Vite dev

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,8 @@ import App from "./App.tsx"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
-async function bootstrap() {
-  const loaderUrl = `${import.meta.env.BASE_URL}vendor/sheetjs-loader.mjs`
-  await import(/* @vite-ignore */ loaderUrl)
-
-  createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  )
-}
-
-void bootstrap()
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
